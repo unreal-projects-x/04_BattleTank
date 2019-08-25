a99c028665174214c35d771d3ed7dd01fa4eb17e
JosTheBossX <yashstevenfinn@gmail.com>
2019-08-25 17:22:48 +0530

    BT 21 Using virtual and override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,9 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "BattleTank.h"
+
 
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
+	
 	return Cast<ATank>(GetPawn());
+
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possessing: %s"),*(ControlledTank->GetName()));
+	}
 }
