65f1fbbbe2de7aac1ef74dfaeb704291f38d6303
JosTheBossX <yashstevenfinn@gmail.com>
2019-08-27 20:17:50 +0530

    BT 26 Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -37,7 +37,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; // out parameter
 	if (GetSightRayHitlocation(HitLocation)) //Has"Side-effect", is going to line trace
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
+		//UE_LOG(LogTemp, Warning, TEXT("Look Direction: %s"), *HitLocation.ToString())
 
 				//TODO tell controlled tank to aim at this point
 	}
@@ -46,6 +46,11 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 bool ATankPlayerController::GetSightRayHitlocation(FVector& OutHitLocation) const
 {
-	OutHitLocation = FVector(1.0);
+	//Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation , ViewportSizeY * CrossHairYLocation);
+	//"De-project" the screen position of the crosshair to a world direction
+	//line-trace along that look direction, and see what we hit(up to max range)
 	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -16,17 +16,22 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
+	
 
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float Deltatime) override;
 	//start the tank moving the barrel so that a shot would hit where 
 	//the crosshair intersects the world
-	void AimTowardsCrosshair();
+	
 
 
 private:
-
+	ATank* GetControlledTank() const;
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
+	void AimTowardsCrosshair();
 	bool GetSightRayHitlocation(FVector& OutHitLocation) const;
 };
